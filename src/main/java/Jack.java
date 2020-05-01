895195367fa4609eae5f0e4b318a6330161b9626
clicking buttons <clickingbuttons@pm.me>
2020-04-30 21:23:37 -0400

    run aggs and grouped

--- src/main/java/Jack.java
+++ src/main/java/Jack.java
@@ -11,11 +11,11 @@ public class Jack {
         Calendar to = new GregorianCalendar();
 //        Calendar from = new GregorianCalendar(2004, Calendar.JANUARY, 1);
 //        Calendar to = new GregorianCalendar(2005, Calendar.JANUARY, 1);
-//        BackfillAllStats groupedStats = BackfillRange.backfillIndex(from, to, BackfillRange.BackfillMethod.grouped, "");
-//        groupedStats.writeSymbols("agg1d_grouped.csv");
-//        groupedStats.writeJSON("agg1d_grouped_days.json");
         BackfillAllStats aggStats = BackfillRange.backfillIndex(from, to, BackfillRange.BackfillMethod.aggs, "");
         aggStats.writeSymbols("agg1d_aggs.csv");
         aggStats.writeJSON("agg1d_aggs_days.json");
+        BackfillAllStats groupedStats = BackfillRange.backfillIndex(from, to, BackfillRange.BackfillMethod.grouped, "");
+        groupedStats.writeSymbols("agg1d_grouped.csv");
+        groupedStats.writeJSON("agg1d_grouped_days.json");
     }
 }
